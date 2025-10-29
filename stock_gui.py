4f28f71db90d4339a674964acebfc192004f39d0
suchengzi99 <hotchise@sina.com>
2025-10-29 20:37:53 +0800

    2

--- stock_gui.py
+++ stock_gui.py
@@ -10,6 +10,7 @@ from miniqmt_duckdb import MiniqmtDuckDB
 # 配置日志
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
+#test
 
 class StockGUI:
     def __init__(self, root):
